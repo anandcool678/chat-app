edf5cb2004a6a7c20a19735174859023518b67e5
anandcool678 <anand201101dwivedi@gmail.com>
2023-07-30 17:56:48 +0530

    mongo db connected

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -7,11 +7,15 @@ const userSchema = mongoose.Schema({
         },
         email:{
             type: String,
-            required: true
+            trim: true,
+            required: true,
+            lowercase: true,
+            match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please add valid email address.']
         },
         password:{
             type: String,
-            required: true
+            required: true,
+            select: false
         },
         pic:{
             type: String,
