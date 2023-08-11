60b698d7c55b2f3fb779e78f24084780aceac0db
anandcool678 <anand201101dwivedi@gmail.com>
2023-08-11 12:05:26 +0530

    chat Controller done

--- Backend/controllers/userControllers.js
+++ Backend/controllers/userControllers.js
@@ -24,10 +24,10 @@ const registerUser =asyncHandler(async(req, res) => {
     });
 
     if(user){
-        res.status(201).json({
+        res.json({
             _id: user._id,
             name: user.name,
-            password: user.password,
+            email: user.email,
             pic: user.pic,
             token: generateToken(user._id)
         });
@@ -58,8 +58,21 @@ const authUser = asyncHandler(async(req, res) => {
     }
 });
 
+//  /api/user?search=abc
+const allUsers = asyncHandler(async(req, res) => {
+    const keyword = req.query.search?{
+        $or: [
+            {name: {$regex: req.query.search, $options: 'i'}},
+            {email: {$regex: req.query.search, $options: 'i'}},
+        ]
+    }:{};
+
+    const users = await User.find(keyword).find({_id:{$ne: req.user._id}});
+    res.send(users );
+
+});
 
 
-module.exports = {registerUser, authUser};
+module.exports = {registerUser, authUser, allUsers};
 
 
